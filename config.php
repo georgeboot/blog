8bc852747dfe3359c5d130cbd80438915e2854ef
George Boot <884482+georgeboot@users.noreply.github.com>
2021-07-22 14:12:43 +0200

    Add george image

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Str;
 
 return [
-    'baseUrl' => '',
+    'baseUrl' => 'http://localhost:3000',
     'production' => false,
     'siteName' => 'George Boot',
     'siteDescription' => 'Everything and what not',
