8534796dbdab2be8111138d061e9c3a2dce83fba
George Boot <884482+georgeboot@users.noreply.github.com>
2021-07-27 17:01:38 +0200

    Hide email from spambots

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -14,8 +14,14 @@ description: Get in contact with George Boot
         <ul class="list-inside">
             <li>Twitter: <a target="_blank" rel="noopener"href="https://twitter.com/georgeboot">@georgeboot</a> <span class="text-sm text-gray-500">(be careful, I enjoy politics too...)</span></li>
             <li>Telegram: <a target="_blank" rel="noopener" href="https://t.me/georgeboot">@georgeboot</a></li>
-            <li>E-mail: <a target="_blank" rel="noopener" href="mailto:georgeboot@icloud.com">georgeboot@icloud.com</a></li>
+            <li>E-mail: <a target="_blank" rel="noopener" data-obfuscated-email="{{ base64_encode('georgeboot@icloud.com') }}"><i>email hidden from spam bots</i></a></li>
         </ul>
+        <script>
+            const el = document.querySelector('a[data-obfuscated-email]')
+            const email = atob(el.getAttribute('data-obfuscated-email'))
+            el.innerHTML = email
+            el.setAttribute('href', `mailto:${email}`)
+        </script>
     </article>
 
 @stop
